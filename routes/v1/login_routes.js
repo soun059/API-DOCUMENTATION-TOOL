23875a5f3ee117ceb21ead9280efb239a5b4da60
soun059 <soujanna2015newera@gmail.com>
2019-12-18 17:36:44 +0530

    new commit

--- routes/v1/login_routes.js
+++ routes/v1/login_routes.js
@@ -1,51 +1,62 @@
 module.exports = {
-    configure: function (app, mongo, ObjectID, url, assert, dbb, db) {
-        var login_module = require('../../models/v1/login_module')(mongo, ObjectID, url, assert, dbb, db);
-        
-        // API Login User
-        // headers:
-        // params: email, password
-        // Functions: login_user
-        // Response: result, error, message
-        app.post('/login_user', function (req, res) {
-            try {
-                if (req.body.hasOwnProperty("email") && req.body.hasOwnProperty("password")) {
-                    
-                    login_module.login_user(req.body.email, req.body.password, function (result, error, message) {
-                        if (error) {
-                            res.json({
-                                status: false,
-                                message: message,
-                            });
-                        } else {
-                            res.json({
-                                status: true,
-                                message: message,
-                                user_token: result
-                            });
-                        }   
-                    });
+  configure: function(app, mongo, ObjectID, url, assert, dbb, db) {
+    var login_module = require("../../models/v1/login_module")(
+      mongo,
+      ObjectID,
+      url,
+      assert,
+      dbb,
+      db
+    );
 
-                } else {
-                    if (req.body.hasOwnProperty("email") == false) {
-                        res.json({
-                            status: false,
-                            message: "email parameter is missing"
-                        });
-                    } else if (req.body.hasOwnProperty("password") == false) {
-                        res.json({
-                            status: false,
-                            message: "password parameter is missing"
-                        });
-                    }
-                }
-            } catch (error) {
-               
-                res.json({
-                    status: false,
-                    message: error
-                })
+    // API Login User
+    // headers:
+    // params: email, password
+    // Functions: login_user
+    // Response: result, error, message
+    app.post("/login_user", function(req, res) {
+      try {
+        if (
+          req.body.hasOwnProperty("email") &&
+          req.body.hasOwnProperty("password")
+        ) {
+          login_module.login_user(req.body.email, req.body.password, function(
+            result,
+            error,
+            message
+          ) {
+            if (error) {
+              res.json({
+                status: false,
+                message: message
+              });
+            } else {
+              res.json({
+                status: true,
+                message: message,
+                user_token: result
+              });
             }
-        })
-    }
-}    
+          });
+        } else {
+          if (req.body.hasOwnProperty("email") == false) {
+            res.json({
+              status: false,
+              message: "email parameter is missing"
+            });
+          } else if (req.body.hasOwnProperty("password") == false) {
+            res.json({
+              status: false,
+              message: "password parameter is missing"
+            });
+          }
+        }
+      } catch (error) {
+        res.json({
+          status: false,
+          message: error
+        });
+      }
+    });
+  }
+};
